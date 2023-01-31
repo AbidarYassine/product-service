809a13d68fe2158e0d981c6a0c0f7dbc5ba54e5f
AbidarYassine <yassine.abidar@edu.uca.ma>
2023-01-31 22:17:33 +0100

    init && add entities

---
+++ src/main/java/io/github/abidaryassine/productservice/service/facade/ProductService.java
@@ -0,0 +1,13 @@
+package io.github.abidaryassine.productservice.service.facade;
+
+import io.github.abidaryassine.productservice.entities.ProductEntity;
+
+/**
+ * @author yassineabidar on 31/1/2023
+ */
+public interface ProductService {
+
+
+    ProductEntity save(ProductEntity product);
+
+}
